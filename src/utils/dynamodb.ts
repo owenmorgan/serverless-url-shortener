aba76af6a3ebd5f093af09e36de55a411971616a
Owen Morgan <osmorgan@gmail.com>
2023-02-19 18:50:29 +0000

    single table design

--- src/utils/dynamodb.ts
+++ src/utils/dynamodb.ts
@@ -2,6 +2,7 @@ import { ShortURL } from "../types/shorturl";
 import { DynamoDB } from "aws-sdk";
 
 const linkTable = process.env.LINK_TABLE as string;
+
 const dynamodb = new DynamoDB.DocumentClient();
 
 export async function putShortUrl(shortURL: ShortURL): Promise<boolean> {
@@ -12,6 +13,8 @@ export async function putShortUrl(shortURL: ShortURL): Promise<boolean> {
     .put({
       TableName: linkTable,
       Item: {
+        type: "SHORT_URL",
+        sk: `id#${shortURL.id}`,
         ...shortURL,
       },
       ConditionExpression: "attribute_not_exists(id)",
@@ -30,7 +33,8 @@ export async function getShortUrl(id: string): Promise<ShortURL | null> {
       .get({
         TableName: linkTable,
         Key: {
-          id,
+          type: "SHORT_URL",
+          sk: `id#${id}`,
         },
       })
       .promise();
@@ -44,10 +48,13 @@ export async function incrementClick(shortURL: ShortURL): Promise<boolean> {
     throw new Error("'LINK_TABLE' Environment Variable Not Set");
   }
   shortURL.clicks++;
+  console.log(JSON.stringify(shortURL));
   await dynamodb
     .put({
       TableName: linkTable,
       Item: {
+        type: "SHORT_URL",
+        sk: `id#${shortURL.id}`,
         ...shortURL,
       },
     })
